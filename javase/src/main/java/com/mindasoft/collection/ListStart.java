feac27aad804c94120709ff2f5b6b19d82a2560f
黄敏 <hmiter@sina.com>
2017-11-10 10:30:55 +0800

    spring boot

--- javase/src/main/java/com/mindasoft/collection/ListStart.java
+++
@@ -1,8 +0,0 @@
-package com.mindasoft.collection;
-
-/**
- * Created by huangmin on 2017/9/19 15:57.
- */
-public class ListStart {
-    //List ArrayList  LinkedList
-}
--- javase/src/main/java/com/mindasoft/collection/SetStart.java
+++
@@ -1,9 +0,0 @@
-package com.mindasoft.collection;
-
-/**
- * Created by huangmin on 2017/9/19 15:57.
- */
-public class SetStart {
-
-    // Set HashSet TreeSet
-}
