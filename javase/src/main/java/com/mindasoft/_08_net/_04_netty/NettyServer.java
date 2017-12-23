72715c971a62da4d213f8062e5d141d0bf6bf060
黄敏 <hmiter@sina.com>
2017-12-23 16:42:13 +0800

    添加netty依赖

--- javase/src/main/java/com/mindasoft/_08_net/_04_netty/NettyServer.java
+++ javase/src/main/java/com/mindasoft/_08_net/_04_netty/NettyServer.java
@@ -49,7 +49,6 @@ public class NettyServer {
     }
 
     private class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
-
         @Override
         protected void initChannel(SocketChannel socketChannel) throws Exception {
             socketChannel.pipeline().addLast( new TimeServerHandler());
@@ -57,6 +56,19 @@ public class NettyServer {
     }
 
     public class TimeServerHandler extends ChannelHandlerAdapter{
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            super.exceptionCaught(ctx, cause);
+        }
 
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            super.channelRead(ctx, msg);
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            super.channelReadComplete(ctx);
+        }
     }
 }
