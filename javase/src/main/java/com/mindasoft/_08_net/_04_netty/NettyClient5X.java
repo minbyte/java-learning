fedd3313f86a961533c200e8a9c86386e8655fa8
黄敏 <hmiter@sina.com>
2017-12-27 10:29:05 +0800

    netty5.x写法

--- javase/src/main/java/com/mindasoft/_08_net/_04_netty/NettyClient5X.java
+++ javase/src/main/java/com/mindasoft/_08_net/_04_netty/NettyClient5X.java
@@ -11,37 +11,36 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  * User: huangmin
  * DateTime: 2017/12/25 18:30
  */
-public class NettyClient {
+public class NettyClient5X {
 
     public static int PORT_NUMBER = 1234;
     public static String HOST = "1234";
 
     public static void main(String[] args) throws Exception {
         int port = PORT_NUMBER;
-        if (args.length > 0)
-        { // 覆盖默认的监听端口
+        if (args.length > 0) { // 覆盖默认的监听端口
             port = Integer.parseInt(args[0]);
         }
         System.out.println("Listening on port " + port);
 
-        new NettyClient().bind(port);
+        new NettyClient5X().bind(port);
     }
 
-    private void bind(int port) throws Exception{
+    private void bind(int port) throws Exception {
         EventLoopGroup clientGroup = new NioEventLoopGroup();
         try {
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.group(clientGroup)
                     .channel(NioServerSocketChannel.class)
                     .handler(new MyChannelInitializer())
-                    .option(ChannelOption.SO_BACKLOG,1024);
+                    .option(ChannelOption.SO_BACKLOG, 1024);
 
             // 绑定端口，同步等待成功
-            ChannelFuture future =  bootstrap.connect(HOST,port).sync();
+            ChannelFuture future = bootstrap.connect(HOST, port).sync();
 
             // 等待服务端监听端口关闭
             future.channel().closeFuture().sync();
-        }finally {
+        } finally {
             clientGroup.shutdownGracefully();
         }
     }
@@ -54,7 +53,7 @@ public class NettyClient {
         }
     }
 
-    private class ChannelHandler extends ChannelHandlerAdapter{
+    private class ChannelHandler extends ChannelHandlerAdapter {
         @Override
         public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
             super.exceptionCaught(ctx, cause);
