a67703bb370f219361ef15983734e2e0b14ddb99
huangmin <huangmin@mgtv.com>
2018-07-16 19:12:54 +0800

    beanFactory

--- spring/src/main/java/com/mindasoft/spring/ApplicationContextStart.java
+++ spring/src/main/java/com/mindasoft/spring/ApplicationContextStart.java
@@ -13,5 +13,8 @@ public class ApplicationContextStart {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("");
+        System.out.println("一共加载了bean个数:" + context.getBeanDefinitionCount());
+
+        context.getBean("personServier");
     }
 }
--- spring/src/main/java/com/mindasoft/spring/DefaultListableBeanFactoryStart.java
+++ spring/src/main/java/com/mindasoft/spring/DefaultListableBeanFactoryStart.java
@@ -5,6 +5,16 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
 /**
+ * 在Spring当中有几个主要的类。
+ * Bean最基本的容器
+ * @see org.springframework.beans.factory.BeanFactory
+ * Resource 描述需要加载的Bean
+ * @see org.springframework.core.io.Resource
+ * BeanDefinition 定义Bean的基本数据结构
+ * @see org.springframework.beans.factory.config.BeanDefinition
+ * 从Resource 中读取定义的bean，并解析为BeanDefinition,主要的方法loadBeanDefinitions
+ * @see org.springframework.beans.factory.support.BeanDefinitionReader
+ *
  * @author: huangmin
  * @email: huangmin@mgtv.com
  * @date: 2018/6/25 17:31
@@ -13,7 +23,7 @@ import org.springframework.core.io.ClassPathResource;
 public class DefaultListableBeanFactoryStart {
 
     public static void main(String[] args) {
-        // 1、指定 bean 定义的xml
+        // 1、指定 bean 定义的xml Resource
         ClassPathResource resouce = new ClassPathResource("xml file");
 
         DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
