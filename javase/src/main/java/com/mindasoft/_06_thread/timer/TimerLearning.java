fe871d89a8cb590d442d1854836ef0038714fa0f
huangmin <huangmin@mgtv.com>
2019-04-10 10:43:59 +0800

    timer

--- javase/src/main/java/com/mindasoft/_06_thread/timer/TimerLearning.java
+++ javase/src/main/java/com/mindasoft/_06_thread/timer/TimerLearning.java
@@ -1,5 +1,6 @@
 package com.mindasoft._06_thread.timer;
 
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -9,21 +10,40 @@ import java.util.TimerTask;
  * @date: 2018/6/12 16:52
  * @version: 1.0.0
  *
- *  // 前一次执行程序结束后 执行
- *  // 在循环执行类别中根据循环时间间隔又可以分为两类
+ *  延迟执行一次
+ *  public void schedule(TimerTask task, long delay);
+ *  public void schedule(TimerTask task, Date time);
+ *
+ *  // 前一次执行程序结束后 间隔时间，再执行任务
  *  public void schedule(TimerTask task, long delay, long period) ;
  *  public void schedule(TimerTask task, Date firstTime, long period) ;
  *
- * //前一次程序执行开始后 执行
+ * // 每隔多少时间，固定执行任务。
  *  public void scheduleAtFixedRate(TimerTask task, long delay, long period)
  *  public void scheduleAtFixedRate(TimerTask task, Date firstTime, long period)
  */
-public class TimerStart2 {
+public class TimerLearning {
 
     public static void main(String[] args) {
         Timer timer = new Timer();
 
-        //前一次执行程序结束后 2000ms 后开始执行下一次程序
+        //延迟1000ms执行程序
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("IMP 当前时间" + this.scheduledExecutionTime());
+            }
+        }, 1000);
+
+        //延迟10000ms执行程序
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("IMP 当前时间" + this.scheduledExecutionTime());
+            }
+        }, new Date(System.currentTimeMillis() + 10000));
+
+        //前一次执行程序结束后 间隔2000ms 再开始执行下一次程序
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -31,7 +51,7 @@ public class TimerStart2 {
             }
         }, 0,2000);
 
-        //前一次程序执行开始 后 2000ms后开始执行下一次程序
+        //每间隔2000ms后，执行一次程序
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
